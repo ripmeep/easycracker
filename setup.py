afc52f40368dc193b0638270692a50fa896d8491
meep <36815692+ripmeep@users.noreply.github.com>
2020-11-12 18:23:05 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup, Extension
 
-setup(name="easycracker", version="1.0",
+setup(name="easycracker", version="1.2.1",
 	ext_modules=[
 		Extension(
 			"easycracker", ["easycracker.c"],
-			extra_link_args = ["-lcrypto", "-lcurl"]
+			extra_link_args = ["-lcrypto", "-lcurl", "-lsqlite3"]
 		)
 	]
 )
