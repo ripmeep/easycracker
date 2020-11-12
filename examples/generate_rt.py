c4bf5da5493441e08b061527b8c99871ddd89027
meep <36815692+ripmeep@users.noreply.github.com>
2020-11-12 18:21:29 +0000

    Add files via upload

---
+++ examples/generate_rt.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import easycracker
+import sqlite3
+import random, sys
+from tqdm import tqdm
+
+f = open("top-passwords.lst", "r")
+lines = f.readlines()
+
+rt = easycracker.RainbowDatabase()
+
+try:
+	rt.create("hashes.db")
+except Exception as e:
+	print(str(e))
+	sys.exit()
+
+db = sqlite3.connect("hashes.db")
+cur = db.cursor()
+
+for i in tqdm(range(len(lines))):
+	plaintext = lines[i].strip()
+
+	sys.stdout.write("\r[%s] Generaring Hash Tables "%(random.choice(["|", "/", "-", "\\"])))
+
+	md4hash = easycracker.MD4Hash(plaintext).digest_dict()["hex"]
+	md5hash = easycracker.MD5Hash(plaintext).digest_dict()["hex"]
+	sha1hash = easycracker.SHA1Hash(plaintext).digest_dict()["hex"]
+	sha224hash = easycracker.SHA224Hash(plaintext).digest_dict()["hex"]
+	sha256hash = easycracker.SHA256Hash(plaintext).digest_dict()["hex"]
+	sha384hash = easycracker.SHA384Hash(plaintext).digest_dict()["hex"]
+	sha512hash = easycracker.SHA512Hash(plaintext).digest_dict()["hex"]
+
+	cur.execute(rt.craft_entry(plaintext, md4hash, "MD4"))
+	cur.execute(rt.craft_entry(plaintext, md5hash, "MD5"))
+	cur.execute(rt.craft_entry(plaintext, sha1hash, "SHA1"))
+	cur.execute(rt.craft_entry(plaintext, sha224hash, "SHA224"))
+	cur.execute(rt.craft_entry(plaintext, sha256hash, "SHA256"))
+	cur.execute(rt.craft_entry(plaintext, sha384hash, "SHA384"))
+	cur.execute(rt.craft_entry(plaintext, sha512hash, "SHA512"))
+
+db.commit()
+
---
+++ examples/search_rt.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# This type of attack, you only need a partial hash
+# to find a potential match for plaintext
+# and its really fast
+
+import easycracker
+
+hashes = [
+    "5f4dcc3b5aa765d61", # MD5 "password"
+    "aaf4c61ddcc5e8a2d", # SHA1 "hello"
+	"a2217124a034c5c9a", # SHA224 "goodpassword"
+    "240be518fabd2724d", # SHA256 "admin123" 
+    "548568964fb078e3a", # SHA384 "dragon"
+    "38221f3553236a283", # SHA512 "supersecret"
+]
+
+rt = easycracker.RainbowDatabase()
+rt.load("hashes.db")
+
+for hash in hashes:
+	print("\nSearching for partial hash ({})".format(hash))
+
+	rt.search(hash)
+
+	if rt.results == 0:
+		print("Couldnt find any matches")
+		continue
+
+	for i in range(rt.results):
+		entry = rt.get_result(i)
+
+		plaintext = entry["plaintext"]
+		algorithm = entry["algorithm"]
+
+		print("Potential match => {}:{}\n".format(plaintext, algorithm))
+
