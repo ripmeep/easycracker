d29efd0d59e7ac9d0deff592ce49afac6a715cee
meep <36815692+ripmeep@users.noreply.github.com>
2020-11-14 19:01:18 +0000

    Update search_rt.py

--- examples/search_rt.py
+++ examples/search_rt.py
@@ -2,13 +2,13 @@
 
 # This type of attack, you only need a partial hash
 # to find a potential match for plaintext
-# and its really fast
+# and its really fast.
 
 import easycracker
 
 hashes = [
     "5f4dcc3b5aa765d61", # MD5 "password"
-    "aaf4c61ddcc5e8a2d", # SHA1 "hello"
+	"aaf4c61ddcc5e8a2d", # SHA1 "hello"
 	"a2217124a034c5c9a", # SHA224 "goodpassword"
     "240be518fabd2724d", # SHA256 "admin123" 
     "548568964fb078e3a", # SHA384 "dragon"
