399bebb5ed6878bc5cea201ce22d83961e53580c
meep <36815692+ripmeep@users.noreply.github.com>
2020-11-26 11:49:08 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 clear
 
 sudo apt-get update
-sudo apt-get install python3 python3-pip
-sudo apt-get install libssl-dev libcurl4-openssl-dev libsqlite3-dev
+sudo apt-get install python3 python3-pip -y
+sudo apt-get install libssl-dev libcurl4-openssl-dev libsqlite3-dev -y
 
 sudo python3 setup.py build
 sudo python3 setup.py install
